c0fb9d7a17b49b5274db6248485cba075ecdf4f9
Keval Malde <keval.malde@gmail.com>
2016-03-10 18:40:17 +0530

    First Commit

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var React = require('react');
+
+var About = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<h1>About</h1>
+				<p>
+					This kfdjhfskjh bdfgh
+					<ul>
+						<li>React</li>
+						<li>React 1</li>
+						<li>React 2</li>
+						<li>React 3</li>
+						<li>React 4</li>
+						<li>React 5</li>
+					</ul>
+				</p>
+			</div>
+		);
+	}
+});
+
+module.exports = About;
